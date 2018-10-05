6055d83ab05adbdd9bb26790c54e3ab09b67cfb7
Sam Birjandi <sambirjandi@gmail.com>
2018-10-05 19:33:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var greeting = "Hello, everybody!";
 var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
+
+var topic = "space";
+
+var conversation = "Let's talk about space."
